20ed94072c04f1ce3b93a98bba51cd014a455f46
senay berhe <tsionberhe@gmail.com>
2020-09-26 13:46:20 -0700

    created url and settings

--- educa/settings.py
+++ educa/settings.py
@@ -25,7 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Courses.apps.CoursesConfig',
+    'courses.apps.CoursesConfig',
 ]
 
 MIDDLEWARE = [
