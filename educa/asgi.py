c3285e5ce25e730d75760220e4b1af922d162456
senay berhe <tsionberhe@gmail.com>
2020-09-27 09:41:40 -0700

    modified packages

--- educa/asgi.py
+++ educa/asgi.py
@@ -1,16 +1,8 @@
-"""
-ASGI config for educa project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
-"""
-
 import os
+import django 
+from channels.routing import get_default_application
 
-from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'educa.settings')
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'educa.settings.pro')
+django.setup()
 application = get_asgi_application()
